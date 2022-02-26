99aab31b4e85730f151498208f4760cb4e6d73a9
Hsueh-Yuan Chou <maxchou@bu.edu>
2022-02-26 17:05:25 -0500

    Able to insert user credentials from postman

--- server/index.js
+++ server/index.js
@@ -1,34 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3001
+var userRouter = require('./routes/users');
+const mongoose = require('mongoose');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const express = require('express');
 
+const app = express();
+const port = 3001;
 
- 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://411team:2000123@cluster0.hhxsc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+app.use(express.json());
+app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-async function connectDB(){
-  try{
-    await client.connect();
-    console.log("Connected correctly to databse");
-  }
-  catch(err){
-    console.log(err.stack);
-  }
-  finally{
-    await client.close();
-  }
-}
-connectDB().catch(console.dir);
+const dbUrl = "mongodb+srv://411team:2000123@cluster0.hhxsc.mongodb.net/411project?retryWrites=true&w=majority";
 
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-// app.use('/user', userRouter);
+mongoose.connect(dbUrl, (err) => {
+  if (err) console.log(err);
+})
+app.use('/users', userRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -36,4 +27,33 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
+
+
+ 
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://411team:2000123@cluster0.hhxsc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// var db;
+
+// async function connectDB(){
+//   mongoose.connect('mongodb://username:password@host:port/database?options...');
+
+
+//   // try{
+//   //   await client.connect();
+//   //   db = client.db('411project');
+//   //   exports.db;
+//   //   console.log("Connected correctly to databse");
+    
+//   // }
+//   // catch(err){
+//   //   console.log(err.stack);
+//   // }
+//   // finally{
+//   //   await client.close();
+//   // }
+// }
+// connectDB().catch(console.dir);
+
+
