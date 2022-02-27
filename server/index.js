44800ebfc72bdcf1c0b1028aec5f39e7f12c6f67
Hsueh-Yuan Chou <maxchou@bu.edu>
2022-02-26 21:31:48 -0500

    able to register from front end

--- server/index.js
+++ server/index.js
@@ -31,29 +31,3 @@ app.listen(port, () => {
 
 
  
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://411team:2000123@cluster0.hhxsc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// var db;
-
-// async function connectDB(){
-//   mongoose.connect('mongodb://username:password@host:port/database?options...');
-
-
-//   // try{
-//   //   await client.connect();
-//   //   db = client.db('411project');
-//   //   exports.db;
-//   //   console.log("Connected correctly to databse");
-    
-//   // }
-//   // catch(err){
-//   //   console.log(err.stack);
-//   // }
-//   // finally{
-//   //   await client.close();
-//   // }
-// }
-// connectDB().catch(console.dir);
-
-
