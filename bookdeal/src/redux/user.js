18adfdf636a59947659c63499049d05a86745081
Hsueh-Yuan Chou <maxchou@bu.edu>
2022-02-26 22:45:16 -0500

    Able to Login from frontend

---
+++ bookdeal/src/redux/user.js
@@ -0,0 +1,38 @@
+import * as ActionTypes from './actionTypes';
+
+export const User = (state ={
+            isLoading: false,
+            user: null,
+            errMess: null,
+            isAuthenticated: false
+            }, action) => {
+                    switch(action.type) {
+                        case ActionTypes.LOGIN_REQUEST:
+                            return {
+                                ...state,
+                                isLoading: true,
+                                user: action.creds,
+                                errMess: null,
+                                isAuthenticated: false
+                            };
+                        case ActionTypes.LOGIN_SUCCESS:
+                            return{
+                                ...state,
+                                isLoading: false,
+                                user: action.creds,
+                                errMess: null,
+                                isAuthenticated: true
+                            }
+                        case ActionTypes.LOGIN_FAILURE:
+                            return{
+                                ...state,
+                                isLoading: false,
+                                user: null,
+                                errMess: action.message,
+                                isAuthenticated: false
+                            }
+                        default:
+                            return state;
+                    }  
+                }                                  
+                                
